ac2c25c7d2d39e14238ab2bf5119039025760626
Alfishan09 <alfishankhan009@gmail.com>
2026-06-24 21:02:40 +0530

    Update student name and age in Main class

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         Person person =
-                new Student("Rahul", 20, "Java Programming");
+                new Student("Naira", 22, "Java Programming");
 
         person.displayInfo();
     }
